f7c32c174be741357b52e4e6ac50d511bfa3c09c
Susheel kumar vishwakarma <sushilsks5897@gmail.com>
2025-10-07 15:38:22 +0530

    cors error resolve

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://blog-app-delta-taupe.vercel.app/",
+    origin: "https://blog-app-delta-taupe.vercel.app",
     credentials: true
 }))
 // app.use(express.urlencoded({ extended: true }));
